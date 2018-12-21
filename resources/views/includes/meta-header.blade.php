e84df0c29c716910a6e63d16680fed9b44f3e170
Sony Darmawan <sonypiay@mail.com>
2018-12-21 15:49:16 +0700

    On branch localhost

--- resources/views/includes/meta-header.blade.php
+++ resources/views/includes/meta-header.blade.php
@@ -1,7 +1,9 @@
 <meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
+<meta name="description" content="Biznet Wifi adalah Layanan Wi-Fi Turbo 100 Mbps GRATIS untuk pelanggan Biznet Home dan Biznet Metronet.">
+<meta name="keywords" content="Biznet, 5G, WiFi, Wi-Fi, cepat, murah, hotspot, internet" />
+<link rel="shortcut icon" type="image/x-icon" href="{{ asset('images/logo/logo_biznet_wifi.ico') }}">
 <link rel="stylesheet" href="{{ asset('vendor/uikit/css/uikit.min.css') }}" media="screen" />
 <link rel="stylesheet" href="{{ asset('css/app.css') }}" media="screen" />
 <script src="{{ asset('vendor/uikit/js/uikit.min.js') }}"></script>
